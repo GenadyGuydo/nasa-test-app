6f5ceb701c9d1563f56ab276cecac6159cd84fdb
GenadyGuydo <law.guydo@bk.ru>
2024-01-18 17:21:22 +0300

    current date commit

--- src/components/dete_input.jsx
+++ src/components/dete_input.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import '../CSS/input.css'
 const DateInput = ({ label, value, onChange }) => {
+  const today = new Date().toISOString().split('T')[0];
   return (
     <div className='date_gap'>
       <label className='date_label' >{label}:</label>
-      <input className='date_style' type="date" value={value} onChange={onChange} />
+      <input className='date_style' type="date" value={value} onChange={onChange} max={today} />
     </div>
   );
 };
